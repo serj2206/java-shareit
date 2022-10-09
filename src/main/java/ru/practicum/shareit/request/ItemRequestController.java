78ca81911ebc7911a669bcfeda2a3b2819286871
serj2206 <serj2206@yandex.ru>
2022-10-09 08:25:42 +0300

    add-item-request: начало

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,9 +1,15 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
 
 @RestController
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
+
+    @PostMapping
+    public ItemRequestDto addItemRequest(@RequestHeader("X-Sharer-User-Id") long userId,
+                                         @RequestBody ItemRequestDto itemRequestDto) {
+        return null;
+    }
 }
